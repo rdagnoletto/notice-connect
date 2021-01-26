7add19373dd43a2344228987ac44c956d2e1d806
Rob Agnoletto <robagnoletto@Robs-MacBook-Pro.local>
2021-01-26 12:39:14 -0500

    Added forms to views as well as to html templates. Updated git ignore to include db

--- match/match/views.py
+++ match/match/views.py
@@ -1,15 +1,44 @@
 from django.shortcuts import render, redirect
-#from .forms import ContactForm
+from .forms import CreateForm, DeleteForm
 from django.conf import settings
 
 def home(request):
     return render(request,'match/home.html')
 
 def create(request):
-    return render(request,'match/create.html')
+    if request.method == 'POST':
+        form = CreateForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            province = form.cleaned_data['province']
+            date_of_birth = form.cleaned_data['date_of_birth']
+
+            form = CreateForm()
+            message = "Testing"
+            return render(request,'match/create.html' , {'form': form,'message': message})
+
+    else:
+        form = CreateForm()
+
+    return render(request,'match/create.html' , {'form': form})
 
 def delete(request):
-    return render(request,'match/delete.html')
+    if request.method == 'POST':
+        form = DeleteForm(request.POST)
+
+        if form.is_valid():
+            record_id = form.cleaned_data['record_id']
+            
+            form = DeleteForm()
+            message = "Testing"
+            return render(request,'match/delete.html' , {'form': form,'message': message})
+
+    else:
+        form = DeleteForm()
+
+    return render(request,'match/delete.html' , {'form': form})
 
 def matches(request):
     return render(request,'match/matches.html')
