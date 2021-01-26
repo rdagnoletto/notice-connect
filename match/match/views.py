ae37c5fa3bdae70b92f2604c7327b780a063dadb
Rob Agnoletto <robagnoletto@Robs-MacBook-Pro.local>
2021-01-26 15:38:46 -0500

    Added match logic

--- match/match/views.py
+++ match/match/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render, redirect
 from .forms import CreateForm, DeleteForm
 from .models import Record, Match, Notice
 from django.conf import settings
+from django.db.models import Q
+
+def make_matches(record):
+    weak = Notice.objects.filter(Q(first_name=record.first_name)|Q(alt_first_name=record.first_name)).filter(Q(last_name=record.last_name)|Q(alt_last_name=record.last_name))
+    possible = weak.filter(province=record.province)
+    strong = possible.filter(date_of_birth=record.date_of_birth)
+
+    weak = weak.difference(possible)
+    possible = possible.difference(strong)
+
+    for n in strong:
+        match = Match(record=record,notice=n,m_type='S')
+        match.save()
+    
+    for n in possible:
+        match = Match(record=record,notice=n,m_type='P')
+        match.save()
+    
+    for n in weak:
+        match = Match(record=record,notice=n,m_type='W')
+        match.save()
 
 def home(request):
     return render(request,'match/home.html')
@@ -19,7 +40,7 @@ def create(request):
             r = Record(first_name=first_name, last_name=last_name,province=province,date_of_birth=date_of_birth)
             r.save()
             # Make match entries
-            
+            make_matches(r)
             form = CreateForm()
             message = "Record Created Successfully"
             return render(request,'match/create.html' , {'form': form,'message': message})
