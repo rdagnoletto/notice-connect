f7517a1393d187b257ea1c600eb3e36ec5e26cbd
Rob Agnoletto <robagnoletto@Robs-MacBook-Pro.local>
2021-01-27 08:45:06 -0500

    Added last 3 tests for multiple matches and having matches delete with related notices and records

--- match/match/tests.py
+++ match/match/tests.py
@@ -52,16 +52,64 @@ class MatchTestCase(TestCase):
     def test_multiple_matches(self):
         # create record that results in multiple matches
         # confirm the matches are of the correct type and no duplicates
-        pass
+        r = Record(first_name="Rob",  last_name="Age", province="ON", date_of_birth=datetime.date(1992,4,15))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),3)
+        strong = False
+        possible = False
+        weak = False
+        for m in matches:
+            n = m.notice
+            if m.m_type == 'S':
+                strong = True
+                self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+                self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+                self.assertEqual(r.province, n.province)
+                self.assertEqual(r.date_of_birth, n.date_of_birth)
+                self.assertEqual(matches[0].record, r)
+            elif m.m_type == 'P':
+                possible = True
+                self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+                self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+                self.assertEqual(r.province, n.province)
+                self.assertEqual(matches[0].record, r)
+            elif m.m_type == 'W':
+                weak = True
+                self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+                self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+                self.assertEqual(matches[0].record, r)
+        
+        self.assertTrue(strong and possible and weak)
     
     def test_delete_record(self):
         # create record that results in matches and confirm 
         # matches are deleted when the record is deleted
-        pass
+        r = Record(first_name="Rob",  last_name="Age", province="ON", date_of_birth=datetime.date(1992,4,20))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertGreaterEqual(len(matches),1)
+        r.delete()
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),0)
+        
 
     def test_delete_notice(self):
-        # create record that results in a match with a notice and 
-        # confirm the match is deleted when the notice is delete
-        pass
+        # create record that results in matches with and 
+        # confirm the match is deleted when the notices are delete
+        r = Record(first_name="Rob",  last_name="Agnoletto", province="ON", date_of_birth=datetime.date(1992,6,15))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertGreaterEqual(len(matches),1)
+        notices = set()
+        for m in matches:
+            notices.add(m.notice)
+        for n in notices:
+            n.delete()
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),0)
     
     
