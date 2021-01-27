ef62fdc90d4f43fb9158a2af2e9be097378fddb7
Rob Agnoletto <robagnoletto@Robs-MacBook-Pro.local>
2021-01-26 21:13:28 -0500

    Added first 3 tests for single matches

--- match/match/tests.py
+++ match/match/tests.py
@@ -1,18 +1,53 @@
 from django.test import TestCase
+from .models import Record, Notice, Match
+from .views import make_matches
+import datetime
 
 class MatchTestCase(TestCase):
+    fixtures = ['notices.json', ]
 
     def test_strong_match(self):
         # create record that results in one strong match
-        pass
+        r = Record(first_name="Robert",  last_name="Age", province="YK", date_of_birth=datetime.date(1992,4,15))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),1)
+        n = matches[0].notice
+        self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+        self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+        self.assertEqual(r.province, n.province)
+        self.assertEqual(r.date_of_birth, n.date_of_birth)
+        self.assertEqual(matches[0].record, r)
+        self.assertEqual(matches[0].m_type,'S')
 
     def test_possible_match(self):
         # create record that results in one possible match
-        pass 
+        r = Record(first_name="Matt",  last_name="Agnoletto", province="BC", date_of_birth=datetime.date(1991,4,15))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),1)
+        n = matches[0].notice
+        self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+        self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+        self.assertEqual(r.province, n.province)
+        self.assertEqual(matches[0].record, r)
+        self.assertEqual(matches[0].m_type,'P')
+
 
     def test_weak_match(self):
         # create record that results in one weak match
-        pass
+        r = Record(first_name="Tony",  last_name="Agnoletto", province="AB", date_of_birth=datetime.date(1992,4,17))
+        r.save()
+        make_matches(r)
+        matches = Match.objects.all()
+        self.assertEqual(len(matches),1)
+        n = matches[0].notice
+        self.assertTrue(r.first_name in [n.first_name,n.alt_first_name])
+        self.assertTrue(r.last_name in [n.last_name,n.alt_last_name])
+        self.assertEqual(matches[0].record, r)
+        self.assertEqual(matches[0].m_type,'W')
     
     def test_multiple_matches(self):
         # create record that results in multiple matches
